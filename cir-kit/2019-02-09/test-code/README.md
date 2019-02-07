3340d7fb41f87af0ddc7cda1ed8b676fadd04285
Kose-i <tamura.kosei905@mail.kyutech.jp>
2019-02-07 15:18:00 +0900

    Add testcode README

---
+++ cir-kit/2019-02-09/test-code/README.md
@@ -0,0 +1,5 @@
+## テストコード置き場です.
+
+# Write_terminal.cpp
+
+これは,terminalに* open write システムコール*を使用して,出力するコードです.
--- cir-kit/2019-02-09/test-code/Write_terminal.cpp
+++ cir-kit/2019-02-09/test-code/Write_terminal.cpp
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-  int fd = open("/dev/pts/9",O_WRONLY);
+  int fd = open("/dev/pts/9",O_WRONLY);//ps ax | grep bash
   if (fd > 0) {
     std::string tmp{"test-code"};
     int write_ans = write(fd, tmp.c_str(), tmp.size());
